5fb865d706df5aa72bc7ff56474f4cf49ebca66f
Serkan <s_kara12@wwu.de>
2022-10-25 16:48:26 +0200

    unentschieden geht jetzt auch, alles zu lowercase

--- game1.js
+++ game1.js
@@ -20,30 +20,36 @@ getComputerChoice(getRandomzahl()) //das ist random computer ergebnis
 computerSelection = getComputerChoice(getRandomzahl())
 
 function game1 (playerSelection, computerSelection){
-if (playerSelection == "paper" && computerSelection == "scissors"){
-    return "computer won, scissors beats paper."
-}else if (playerSelection == "paper" && computerSelection == "rock"){
+    let playerChoice = String(playerSelection).toLowerCase()
+if (playerChoice == "paper" && computerSelection == "scissors"){
+    return "Computer won, scissors beats paper."
+}else if (playerChoice == "paper" && computerSelection == "rock"){
     return "Player won, paper beats rock"
-}else if (playerSelection == "rock" && computerSelection == "scissors"){
-    return "player won, rock beats scissors"
-}else if (playerSelection == "rock" && computerSelection == "paper"){
-    return "computer won, paper beats rock."
-}else if (playerSelection == "scissors" && computerSelection == "rock"){
-    return "computer won, rock beats scissors."
-}else if (playerSelection == "scissors" && computerSelection == "paper"){
-    return "player won, scissors beats paper."
+}else if (playerChoice == "paper"&& computerSelection == "paper"){
+    return "No winner, paper does not beat paper."
+}else if (playerChoice == "rock" && computerSelection == "scissors"){
+    return "Player won, rock beats scissors"
+}else if (playerChoice == "rock" && computerSelection == "paper"){
+    return "Computer won, paper beats rock."
+}else if (playerChoice == "rock" && computerSelection == "rock"){
+    return "No winner, rock does not beat rock."
+}else if (playerChoice == "scissors" && computerSelection == "rock"){
+    return "Computer won, rock beats scissors."
+}else if (playerChoice == "scissors" && computerSelection == "paper"){
+    return "Player won, scissors beats paper."
+}else if (playerChoice == "scissors" && computerSelection == "scissors"){
+    return "No winner, scissors do not beat scissors."
 }else {
-    return "you must enter rock, paper or scissors."
+    return "You must enter rock, paper or scissors."
 }
 }
 
+console.log(game1 ("Scissors", getComputerChoice(getRandomzahl())))
+console.log(game1 ("Paper", getComputerChoice(getRandomzahl())))
+console.log(game1 ("rOck", getComputerChoice(getRandomzahl())))
+console.log(game1 ("sciSSors", getComputerChoice(getRandomzahl())))
+console.log(game1 ("paPer", getComputerChoice(getRandomzahl())))
+console.log(game1 ("rOCk", getComputerChoice(getRandomzahl())))
 console.log(game1 ("scissors", getComputerChoice(getRandomzahl())))
 console.log(game1 ("paper", getComputerChoice(getRandomzahl())))
-console.log(game1 ("rock", getComputerChoice(getRandomzahl())))
-console.log(game1 ("scissors", getComputerChoice(getRandomzahl())))
-console.log(game1 ("paper", getComputerChoice(getRandomzahl())))
-console.log(game1 ("rock", getComputerChoice(getRandomzahl())))
-console.log(game1 ("scissors", getComputerChoice(getRandomzahl())))
-console.log(game1 ("paper", getComputerChoice(getRandomzahl())))
-
 
